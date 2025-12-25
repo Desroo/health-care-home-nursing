3633d2baf592163762ae4a6644361f452497aa16
Buddhika uditha <buddhikauditha6@gmail.com>
2025-12-25 18:06:21 +0530

    review
    review js

--- server/models/Review.js
+++ server/models/Review.js
@@ -3,11 +3,7 @@ const mongoose = require("mongoose");
 const reviewSchema = new mongoose.Schema(
   {
     name: { type: String, required: true, trim: true },
-    service: {
-      type: String,
-      required: true,
-      enum: ["Baby Sitting", "Elder Care", "Patient Care"],
-    },
+    service: { type: String, required: true, trim: true },
     rating: { type: Number, required: true, min: 1, max: 5 },
     message: { type: String, required: true, trim: true },
     approved: { type: Boolean, default: false }, // admin approves
